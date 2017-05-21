7ca3070b0700eeb4a9fb7909264f2e36df9581ab
Drew Pham <drewvpham@gmail.com>
2017-05-21 17:53:07 -0500

    solution

--- apps/disappearing_ninjas/views.py
+++ apps/disappearing_ninjas/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'disappearing_ninjas/index.html')
+
+def show_ninja(request, color):
+	#ninja got passed in through the url parameter!
+    if color in ['blue','red','orange','purple']:
+        turtle=color
+        context = {'img' : turtle}
+    else:
+        turtle='notapril'
+        context = {'img' : turtle}
+
+    return render(request, 'disappearing_ninjas/ninjas.html', context)
